2f1cee7fde22fe23404f259d94c70f03354be5e4
Garret Thompson <garret.thompson@gmail.com>
2020-09-25 14:49:23 -0400

    Add test for HexFromSDColor

--- choose-tests/choose_tests.m
+++ choose-tests/choose_tests.m
@@ -1,4 +1,7 @@
 #import <XCTest/XCTest.h>
+#import "../SDAppDelegate.m"
+
+static char *colorCode;
 
 @interface choose_tests : XCTestCase
 
@@ -8,22 +11,21 @@
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    colorCode = NULL;
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    if (colorCode != NULL) {
+        free(colorCode);
+    }
 }
 
-- (void)testExample {
+- (void)testHexFromSDColor {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+    colorCode = HexFromSDColor(NSColor.whiteColor);
+    XCTAssert(colorCode != NULL && strcmp(colorCode, "FFFFFF") == 0);
 }
 
 @end
